68be9c78698dc5e441e75b47fb9319df9dbe1133
wenbin <wenbin.shi@gmail.com>
2022-09-06 10:42:26 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bradyjoestar/go-web3-utils
+
+go 1.18
+
+require github.com/onrik/ethrpc v1.1.1
---
+++ src/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/onrik/ethrpc"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("wenbin test")
+
+	client := ethrpc.New("http://127.0.0.1:8545")
+
+	fmt.Println(os.Args[1])
+
+	version, err := client.Web3ClientVersion()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(version)
+
+	if len(os.Args) != 2 {
+		panic("txhash should be provided")
+	}
+
+	tx, err := client.EthGetTransactionByHash(os.Args[1])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(tx)
+}
